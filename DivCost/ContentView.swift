be3fb157c619bb3845488de9255825b82465b4a4
Michał <rusinekek@gmail.com>
2022-09-06 00:54:20 +0200

    Initial Commit

---
+++ DivCost/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DivCost
+//
+//  Created by Michał Rusinek on 06/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DivCost/DivCostApp.swift
@@ -0,0 +1,17 @@
+//
+//  DivCostApp.swift
+//  DivCost
+//
+//  Created by Michał Rusinek on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct DivCostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
